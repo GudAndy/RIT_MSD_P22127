f995bb49d038dea63fb89c339b3b76cba93ff7cb
Andrew Simmons <axs3165@rit.edu>
2022-02-14 01:40:53 +0000

    :)

---
+++ pi_code/comms.py
@@ -0,0 +1,13 @@
+"""
+comms.py: Class file containing methods for communication over I2C
+"""
+
+__author__	= "Andrew Simmons"
+
+#import wiringpi
+import smbus
+
+class Comms:
+	
+	def yay(self):
+		print("yay")
---
+++ pi_code/main.py
@@ -0,0 +1,24 @@
+#!/bin/python
+"""
+main.py: Module for running pi software
+"""
+
+__author__ = "Andrew Simmons"
+
+
+from comms import Comms
+
+
+
+def main():
+	c = Comms()
+	c.yay()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
