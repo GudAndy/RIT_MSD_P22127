77b9b265fcccdc00d091e390907f3ab2b2565b90
Andrew Simmons <axs3165@rit.edu>
2022-04-11 17:39:18 +0100

    Tunable improvement

--- pi_code/kinect.py
+++ pi_code/kinect.py
@@ -1,6 +1,8 @@
 from freenect import sync_get_depth as scan, sync_get_video as get_vid
 import cv2
 import frame_convert2
+import warnings
+
 
 def main():
     print("Kinect Test Code")
@@ -8,7 +10,7 @@ def main():
     depth_sum = 0
     sum_right = 0
     sum_left = 0
-    thresh = 1500
+    thresh = 500
 
     data = scan()
     # print("{0} x {1}".format(len(data[0]), len(data[0][0])))
@@ -18,29 +20,28 @@ def main():
     pc_data = data[0][len(data[0]) // 2::]
     for i in range(len(pc_data) - 1):
         for j in range(len(pc_data[0]) - 1):
-            # if pc_data[i][j] < thresh:
-            if j < (len(pc_data[0]) - 1) // 2:
-                # sum_left += 1
-                depth_sum -=pc_data[i][j]
-            else:
-                # sum_right += 1 
-                depth_sum += pc_data[i][j]
+            if pc_data[i][j] < thresh:
+                if j < (len(pc_data[0]) - 1) // 2:
+                    sum_left += 1
+                    # depth_sum -=pc_data[i][j]
+                else:
+                    sum_right += 1 
+                    # depth_sum += pc_data[i][j]
 
-    print("\n\n\n\n\nDepth Sum: {}".format(depth_sum))
-    # print("\n\n\n\n\n\n")
-    # if sum_right > 0 and sum_left == 0: 
-    if depth_sum > thresh:
+    # print("\n\n\n\n\nDepth Sum: {}".format(depth_sum))
+    print("\n\n\n\n\n\n")
+    if sum_right > 0: 
+    # if depth_sum > thresh:
         print("Rover should turn left")
-    # elif sum_right == 0 and sum_left > 0:
-    elif depth_sum < (0 - thresh):
+    elif sum_left > 0:
+    # elif depth_sum < (0 - thresh):
         print("Rover should turn right")
-    # elif sum_right == 0 and sum_left == 0:
-    else:
+    elif sum_right == 0 and sum_left == 0:
+    # else:
         print("Rover should continue forward")
-    # elif sum_right > 0 and sum_left > 0:
-    #     print("Rover is trapped!")
 
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings('ignore', '.*invalid magic.*', )
     main()
