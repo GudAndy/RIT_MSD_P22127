62f28d09d9d87cbb885a2db3495f897f6141cbd8
Andrew Simmons <axs3165@rit.edu>
2022-05-03 08:28:12 +0100

    Updated obstacle avoidance code

--- pi_code/kinect.py
+++ pi_code/kinect.py
@@ -1,46 +1,56 @@
+#!/usr/bin/env python
+
 from freenect import sync_get_depth as scan, sync_get_video as get_vid
 import cv2
 import frame_convert2
 import warnings
+import timeit
+# PID Tuning variables
+kp = .5
+kd = .5
+ki = .5
+
+p, i, d = 0, 0, 0
+
+def control():
+    left, right = kinect_scan()
+    print("\n\n\n\n\n\n")
+    print("sum_left: {}\nsum_right: {}".format(left, right))
+    if right > left: 
+        print("Rover should turn left")
+    elif left > right:
+        print("Rover should turn right")
+    elif right == 0 and left == 0:
+        print("Rover should continue forward")
+
+    print("\n\n\n\n\n\n")
 
 
-def main():
+def kinect_scan():
     print("Kinect Test Code")
 
-    depth_sum = 0
     sum_right = 0
     sum_left = 0
     thresh = 500
 
     data = scan()
-    # print("{0} x {1}".format(len(data[0]), len(data[0][0])))
-    # print(data[0])
-    # print(data[0][-1])
-
+    start = timeit.default_timer()
     pc_data = data[0][len(data[0]) // 2::]
     for i in range(len(pc_data) - 1):
         for j in range(len(pc_data[0]) - 1):
             if pc_data[i][j] < thresh:
                 if j < (len(pc_data[0]) - 1) // 2:
                     sum_left += 1
-                    # depth_sum -=pc_data[i][j]
                 else:
                     sum_right += 1 
-                    # depth_sum += pc_data[i][j]
 
-    # print("\n\n\n\n\nDepth Sum: {}".format(depth_sum))
-    print("\n\n\n\n\n\n")
-    if sum_right > 0: 
-    # if depth_sum > thresh:
-        print("Rover should turn left")
-    elif sum_left > 0:
-    # elif depth_sum < (0 - thresh):
-        print("Rover should turn right")
-    elif sum_right == 0 and sum_left == 0:
-    # else:
-        print("Rover should continue forward")
+    end = timeit.default_timer()
+    print("Total time: {}".format(end-start))
+    return sum_left, sum_right
 
 
+def main():
+    control()
 
 if __name__ == "__main__":
     warnings.filterwarnings('ignore', '.*invalid magic.*', )
